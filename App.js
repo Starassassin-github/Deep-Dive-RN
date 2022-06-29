436612fc2d3193da7214f8f933dfaa5358e81bac
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-06-29 16:11:21 +0700

    04-GettingStart-GameLogic-SwitchingScreen
    04-GettingStart-GameLogic-SwitchingScreen

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StyleSheet, ImageBackground } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import Colors from './constants/colors';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
+
+  if (userNumber) {
+    screen = <GameScreen />
+  }
+
   return (
-    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <ImageBackground 
+    <LinearGradient colors={[ Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
+      <ImageBackground
         source={require('./assets/images/background.jpeg')}
         resizeMode="cover"
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        <StartGameScreen />
+        <SafeAreaView style={styles.rootScreen}>
+          {screen}
+        </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -23,6 +40,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   backgroundImage: {
-    opacity: 0.15,
+    opacity: 0.15
   }
 });
